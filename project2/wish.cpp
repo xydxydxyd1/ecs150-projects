ea343292fef6fbed5f0d704866c8425d4d1e98a2
Eric Xu <xydxydxyd1@gmail.com>
2024-10-07 15:51:40 -0700

    Setup builtin commands

--- project2/wish.cpp
+++ project2/wish.cpp
@@ -1,6 +1,9 @@
 // `wish`, the Wisconsin Shell
 #include <iostream>
 #include <string>
+#include <vector>
+#include <unordered_map>
+#include <functional>
 using namespace std;
 
 const string PROMPT = "wish> ";
@@ -13,12 +16,32 @@ void interactive() {
     while (is_running) {
         cout << PROMPT;
         getline(cin, input);
-        cout << input << endl;
     }
 }
 
 /// Main program in batch mode
 void batch(char** filenames, int num_files) {
+}
+
+// Commands
+
+void builtin_ls(string) {
+}
+
+typedef function<void(string)> command;
+
+unordered_map<string, command> builtin_cmds{
+    {"ls", builtin_ls},
+};
+
+/// Run `cmds`
+void run(const vector<string> cmds, const ostream& output) {
+    for (string cmd : cmds) {
+        try {
+            command builtin_cmd = builtin_cmds.at(cmd);
+            continue;
+        } catch(out_of_range err) {}
+    }
 }
 
 int main(int argc, char** argv) {

3d0f71299dc7cbaf19e731e8da3a7564e87be7d7
Eric Xu <xydxydxyd1@gmail.com>
2024-11-29 22:01:15 -0800

    refactor ds3ls

--- project4/gunrock_web/ds3ls.cpp
+++ project4/gunrock_web/ds3ls.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
-#include <string>
-#include <vector>
 #include <algorithm>
 #include <cstring>
-#include <memory>
 #include <exception>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
 
-#include "StringUtils.h"
-#include "LocalFileSystem.h"
 #include "Disk.h"
+#include "LocalFileSystem.h"
+#include "StringUtils.h"
 #include "ufs.h"
 
 using namespace std;
 
 bool compareByName(const dir_ent_t& a, const dir_ent_t& b) {
-    return std::strcmp(a.name, b.name) < 0;
+  return std::strcmp(a.name, b.name) < 0;
 }
 
 void err() {
@@ -45,21 +45,19 @@ vector<dir_ent_t> resolve_path(LocalFileSystem& fs, string path) {
     throw invalid_argument("path must be absolute (start with '/')");
   }
 
-  int curr_i_num = 0; // corresponds to '/'
+  int curr_i_num = 0;  // corresponds to '/'
   string child_name;
   for (size_t i = 1; i < path.size(); i++) {
     if (path[i] == '/') {
       curr_i_num = fs.lookup(curr_i_num, child_name);
       if (curr_i_num < 0)
-        throw invalid_argument("failed to lookup" + child_name);
+        throw invalid_argument("failed to lookup " + child_name);
       child_name.clear();
-    }
-    else
+    } else
       child_name.push_back(path[i]);
   }
-  // handle no trailing '/'
-  bool has_trailling = child_name.empty();
-  if (!has_trailling) {
+  bool has_trailing_delim = child_name.empty();
+  if (!has_trailing_delim) {
     curr_i_num = fs.lookup(curr_i_num, child_name);
     if (curr_i_num < 0)
       throw invalid_argument("failed to lookup " + child_name);
@@ -72,9 +70,8 @@ vector<dir_ent_t> resolve_path(LocalFileSystem& fs, string path) {
     out.resize(inode.size / sizeof(dir_ent_t));
     if (fs.read(curr_i_num, out.data(), inode.size))
       throw invalid_argument("found file but can't read");
-  }
-  else if (inode.type == UFS_REGULAR_FILE) {
-    if (has_trailling)
+  } else if (inode.type == UFS_REGULAR_FILE) {
+    if (has_trailing_delim)
       throw invalid_argument("regular file cannot have trailing '/'");
     dir_ent_t entry;
     strcpy(entry.name, child_name.c_str());
@@ -84,7 +81,7 @@ vector<dir_ent_t> resolve_path(LocalFileSystem& fs, string path) {
   return out;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   if (argc != 3) {
     cerr << argv[0] << ": diskImageFile directory" << endl;
     cerr << "For example:" << endl;
@@ -93,17 +90,16 @@ int main(int argc, char *argv[]) {
   }
 
   // parse command line arguments
-  unique_ptr<Disk> disk(new Disk(argv[1], UFS_BLOCK_SIZE));
-  unique_ptr<LocalFileSystem> fileSystem(new LocalFileSystem(disk.get()));
+  Disk disk(argv[1], UFS_BLOCK_SIZE);
+  LocalFileSystem fileSystem(&disk);
   string directory = string(argv[2]);
 
   int curr_inode_num = 0;
   inode_t curr_inode;
-  if (fileSystem->stat(curr_inode_num, &curr_inode))
-    err();
+  if (fileSystem.stat(curr_inode_num, &curr_inode)) err();
   vector<dir_ent_t> dir_ents;
   try {
-      dir_ents = resolve_path(*fileSystem, directory);
+    dir_ents = resolve_path(fileSystem, directory);
   } catch (invalid_argument& e) {
     err();
   }
